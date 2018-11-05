efa6e0b70dd9db27073667505292f7ae9364febd
cross-ts <ts.kouhei@gmail.com>
2018-11-05 18:56:45 +0900

    flagの処理をinitに寄せた

--- xlsx2csv.go
+++ xlsx2csv.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "hoge")
+	}
+	flag.Parse()
+}
+
 func main() {
-	fmt.Println("xlsx to csv!")
+	xlsx2csv()
 	os.Exit(0)
+}
+
+func xlsx2csv() {
 }
